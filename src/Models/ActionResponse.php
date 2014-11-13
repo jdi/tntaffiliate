59e8965bfd8d2d7a8c5b751aec93b31a6cf85c1f
Brooke Bryan <brooke@bajb.net>
2014-11-13 14:42:02 +0000

    Support affiliate information on trigger

--- src/Models/ActionResponse.php
+++ src/Models/ActionResponse.php
@@ -7,12 +7,22 @@ class ActionResponse
 {
   protected $_actionId;
   protected $_pixels;
+  protected $_affiliate;
+  protected $_campaign;
+  protected $_sid1;
+  protected $_sid2;
+  protected $_sid3;
 
   public function __construct(ApiResult $result)
   {
-    $res             = $result->getResult();
-    $this->_actionId = isset($res->actionId) ? $res->actionId : null;
-    $this->_pixels   = isset($res->pixels) ? $res->pixels : null;
+    $res              = $result->getResult();
+    $this->_actionId  = isset($res->actionId) ? $res->actionId : null;
+    $this->_pixels    = isset($res->pixels) ? $res->pixels : null;
+    $this->_sid1      = isset($res->sid1) ? $res->sid1 : null;
+    $this->_sid2      = isset($res->sid2) ? $res->sid2 : null;
+    $this->_sid3      = isset($res->sid3) ? $res->sid3 : null;
+    $this->_affiliate = isset($res->affiliate) ? $res->affiliate : null;
+    $this->_campaign  = isset($res->campaign) ? $res->campaign : null;
   }
 
   public function getActionId()
@@ -31,5 +41,30 @@ class ActionResponse
       }
     }
     return $pixels;
+  }
+
+  public function getAffiliate()
+  {
+    return $this->_affiliate;
+  }
+
+  public function getCampaign()
+  {
+    return $this->_campaign;
+  }
+
+  public function getSid1()
+  {
+    return $this->_sid1;
+  }
+
+  public function getSid2()
+  {
+    return $this->_sid2;
+  }
+
+  public function getSid3()
+  {
+    return $this->_sid3;
   }
 }
